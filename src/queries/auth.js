4189003ca1682a8b99bb83f2ba74786f8e003b17
Sarmed <sarmedrizvi@gmail.com>
2022-01-05 16:52:17 +0500

    fixes

--- src/queries/auth.js
+++ src/queries/auth.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { BASE_URL } from 'constants';
+import { BASE_URL } from 'constants/index';
 
 // Design Queries and Mutations Start
 
 export const REGISTER = async (e) => {
-  const res = await axios.post(BASE_URL + `/admin-register`, e);
+  const res = await axios.post(`${BASE_URL}/admin-register`, e);
   return res.data;
 };
 
 export const LOGIN = async (e) => {
-  const res = await axios.post(BASE_URL + `/admin-login`, e);
+  const res = await axios.post(`${BASE_URL}/admin-login`, e);
   return res.data;
 };
