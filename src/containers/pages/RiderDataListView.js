f94c3d578750b88e53b09afc95e220e9f63cb940
SidraAbdullah <sidraabdullah56@gmail.com>
2022-02-18 03:02:30 +0500

    updates

--- src/containers/pages/RiderDataListView.js
+++ src/containers/pages/RiderDataListView.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Card, CustomInput, Badge } from 'reactstrap';
+import { Card, CustomInput, Badge, Button } from 'reactstrap';
 import classnames from 'classnames';
 import { ContextMenuTrigger } from 'react-contextmenu';
 import { Colxx } from 'components/common/CustomBootstrap';
+import IntlMessages from 'helpers/IntlMessages';
 
-const RiderDataListView = ({ rider, isSelect, collect, onCheckItem }) => {
+const RiderDataListView = ({ rider, isSelect, collect }) => {
   return (
     <Colxx xxs="12" className="mb-3">
       <ContextMenuTrigger id="menu_id" data={rider._id} collect={collect}>
         <Card
-          onClick={(event) => onCheckItem(event, rider._id)}
           className={classnames('d-flex flex-row', {
             active: isSelect,
           })}
         >
           <div className="pl-2 d-flex flex-grow-1 min-width-zero">
             <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
-              <p className="list-item-heading mb-1 w-15 w-sm-100 truncate">
+              <p className="list-item-heading mb-1 w-10 w-sm-100 truncate">
                 {rider.name}
               </p>
-              <p className="list-item-heading mb-1 w-15 w-sm-100 truncate">
+              <p className="list-item-heading mb-1 w-20 w-sm-100 truncate">
                 {rider.availableForDelivery
                   ? 'Available for delivery'
                   : 'Not Available for delivery'}
@@ -27,10 +27,22 @@ const RiderDataListView = ({ rider, isSelect, collect, onCheckItem }) => {
               <p className="mb-1 text-muted text-small w-15 w-sm-100">
                 Phone Number: <br /> {rider.phone_number}
               </p>
-              <div className="w-15 w-sm-100">
-                <Badge color={rider.statusColor} pill>
-                  {rider.status}
-                </Badge>
+
+              <div className="w-25 w-sm-100">
+                {rider.status === 'ACTIVE' ? (
+                  <Badge color={rider.statusColor} pill>
+                    {rider.status}
+                  </Badge>
+                ) : (
+                  <>
+                    <Button className='mr-2' color="primary">
+                      <IntlMessages id="Approve" />
+                    </Button>
+                    <Button color="secondary">
+                      <IntlMessages id="Cancel" />
+                    </Button>
+                  </>
+                )}
               </div>
             </div>
 
